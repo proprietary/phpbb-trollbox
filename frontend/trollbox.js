166cceb99d8a091c4b0e0c685eb92931b0df8548
Zelly Snyder <zelly@outlook.com>
2020-11-04 20:25:45 -0800

    improve web components frontend

--- frontend/trollbox.js
+++ frontend/trollbox.js
@@ -1,20 +1,37 @@
 'use strict';
 
+const trollboxMessageRowTemplate = document.createElement('template');
+trollboxMessageRowTemplate.innerHTML = `
+<style>
+time {
+  color: gray;
+}
+</style>
+<p>
+  <strong class="author"></strong>: <span class="message"></span> <time></time>
+</p>
+`
+
 class TrollboxMessageRow extends HTMLElement {
 	static get observedAttributes() {
-		return ["author", "message"];
+		return ["author", "message", "timestamp"];
 	}
 
 	constructor() {
 		super();
 		const shadow = this.attachShadow({mode: 'open'});
-		shadow.innerHTML = `<p><span class="author"></span>: <span class="message"></span></p>`
+		shadow.appendChild(trollboxMessageRowTemplate.content.cloneNode(true));
 	}
 
 	connectedCallback() {
 		const shadow = this.shadowRoot;
 		shadow.querySelector('.author').textContent = this.getAttribute('author');
 		shadow.querySelector('.message').textContent = this.getAttribute('message');
+		const t = new Date(this.getAttribute('timestamp'));
+		const dt = shadow.querySelector('time');
+		dt.setAttribute('datetime', t.toISOString());
+		dt.textContent = t.toLocaleString();
+		
 	}
 
 	attributeChangedCallback(name, oldValue, newValue) {
@@ -39,11 +56,27 @@ const trollboxInputTemplate = document.createElement('template');
 trollboxInputTemplate.innerHTML = `
 <style>
   :host {
-    display: inline-block;
+    display: block;
+  }
+
+  textarea {
+    font-size: 1.5em;
+    flex-basis: 100%;
+  }
+
+  button {
+    font-size: 1.5rem;
+  }
+
+  form {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-end;
+    width: 100%;
   }
 </style>
-<form>
-  <input type="text" required placeholder="Write your message here" />
+<form validate>
+  <textarea required placeholder="Write your message here"></textarea>
   <button type="submit">Send</button>
 </form>`;
 
@@ -56,6 +89,7 @@ class TrollboxInput extends HTMLElement {
 
 	connectedCallback() {
 		this.shadowRoot.querySelector('form').addEventListener('submit', this._handleSubmit);
+		this.shadowRoot.querySelector('textarea').addEventListener('keydown', this._handleKeyPress);
 	}
 
 	disconnectedCallback() {
@@ -64,13 +98,28 @@ class TrollboxInput extends HTMLElement {
 
 	_handleSubmit(evt) {
 		evt.preventDefault();
-		const messageInputField = evt.target.querySelector('input');
+		evt.target.checkValidity();
+		const messageInputField = evt.target.querySelector('textarea');
 		const message = messageInputField.value + '';
-		console.info(`submitting: ${evt.target.querySelector('input').value}`);
+		console.info(`submitting: ${evt.target.querySelector('textarea').value}`);
 		messageInputField.value = '';
 		this.dispatchEvent(
 			new CustomEvent('TrollboxSubmitMessage', { bubbles: true, composed: true, detail: { message: message } })
 		);
+	}
+
+	_handleKeyPress = (evt) => {
+		// Submit the chat message when user presses Enter.
+		// Ignores Shift+Enter, which writes the newline as expected into the textarea.
+		if ((evt.which === 13 || evt.key === 'Enter') && !evt.shiftKey) {
+			// prevent insertion of literal newline
+			evt.preventDefault();
+			// Do not submit on empty input
+			if (evt.target.reportValidity() === true) {
+				// Submit form
+				evt.target.form.dispatchEvent(new Event('submit', { cancelable: true }));
+			}
+		}
 	}
 }
 
@@ -78,11 +127,44 @@ customElements.define('tb-input', TrollboxInput);
 
 const trollboxTemplate = document.createElement('template');
 trollboxTemplate.innerHTML = `
-<div>
-  <h2>Trollbox</h2>
-  <div data-id="messages">
-  </div>
-  <tb-input></tb-input>
+<style>
+:host {
+  display: block;
+}
+
+* {
+  font-family: sans-serif;
+}
+
+.container {
+  background-color: maroon;
+  border-radius: 1rem;
+  color: white;
+  padding: 0.5rem 1rem 1rem 1rem;
+}
+
+[data-id="messages"] {
+  overflow-y: scroll;
+  height: 300px;
+}
+
+header {
+  cursor: pointer;
+}
+
+main tb-input {
+  padding-top: 1rem;
+}
+</style>
+<div class="container">
+  <header>
+    <h2>Trollbox</h2>
+  </header>
+  <main>
+    <div data-id="messages">
+    </div>
+    <tb-input></tb-input>
+  </main>
 </div>
 `;
 
@@ -95,6 +177,7 @@ class Trollbox extends HTMLElement {
 
 	connectedCallback() {
 		this.shadowRoot.addEventListener('TrollboxSubmitMessage', this._handleSubmitMessage);
+		this.shadowRoot.querySelector('header').addEventListener('click', this._toggleVisibility);
 	}
 
 	disconnectedCallback() {
@@ -105,13 +188,32 @@ class Trollbox extends HTMLElement {
 		const newMessageElement = document.createElement('tb-message-row');
 		newMessageElement.setAttribute('author', msg.author);
 		newMessageElement.setAttribute('message', msg.message);
-		this.shadowRoot.querySelector('[data-id="messages"]').appendChild(newMessageElement);
+		newMessageElement.setAttribute('timestamp', msg.timestamp);
+		const messagesContainer = this.shadowRoot.querySelector('[data-id="messages"]');
+		messagesContainer.appendChild(newMessageElement);
+		// scroll down to the latest message
+		messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
 	}
 
 	_handleSubmitMessage = (evt) => {
 		evt.stopPropagation();
 		console.info(`received message in parent: ${evt.detail.message}`);
-		this._addMessage({ author: 'me (TODO)', message: evt.detail.message });
+		this._addMessage({
+			author: 'me (TODO)',
+			message: evt.detail.message,
+			timestamp: new Date().toISOString(),
+		});
+	}
+
+	_toggleVisibility = (evt) => {
+		const body = this.shadowRoot.querySelector('main');
+		if (body.style.visibility === '' || body.style.visibility === 'visible') {
+			body.style.visibility = 'hidden';
+			body.style.maxHeight = '10px';
+		} else if (body.style.visibility === 'hidden') {
+			body.style.visibility = 'visible';
+			body.style.maxHeight = '';
+		}
 	}
 }
 
